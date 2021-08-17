2478f1a7506a1218b0748fb6988a04974838a679
David Mwendwa <klatzdavie19@gmail.com>
2021-08-17 22:12:24 +0300

    feat: add grid to the projects section

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -5,14 +5,21 @@ import './Layout.scss'
 class Layout extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="navbar">Navbar</div>
+      <div className='container'>
+        <div className='navbar'>Navbar</div>
         <div className='header'>Header</div>
         <div className='header-side'>Side</div>
         <div className='features'>features</div>
         <div className='about__pictures'>about pictures</div>
         <div className='about__content'>about content</div>
-        <div className='projects'>Projects</div>
+        <div className='projects'>
+          <div>Project one</div>
+          <div>Project 2</div>
+          <div>Project 3</div>
+          <div>Project 4</div>
+          <div>Project 5</div>
+          <div>Project 6</div>
+        </div>
         <div className='contacts'>Contacts</div>
         <div className='footer'>Footer</div>
       </div>
